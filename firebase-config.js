37bfa024b2000a1cb95cd1142e930d4a6527de89
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2025-10-08 11:49:28 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase web config for your project. Loaded by index.html before firebase.js
+// Note: Do not commit secrets you don't want public when using GitHub Pages.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyCWmdF1yTeYZhFGcGp2JIc34etqyNe_f74",
+  authDomain: "tazmartenagh.firebaseapp.com",
+  projectId: "tazmartenagh",
+  storageBucket: "tazmartenagh.firebasestorage.app",
+  messagingSenderId: "754336580475",
+  appId: "1:754336580475:web:3f0633ee0778f126e333de",
+  measurementId: "G-XJV502GGWK"
+};
+
+
