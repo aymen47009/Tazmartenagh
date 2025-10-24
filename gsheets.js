1786e7c8e49febf5bb1eba6b481f6c0ee15d4f7f
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2025-10-24 15:19:12 +0100

    Update gsheets.js

--- gsheets.js
+++ gsheets.js
@@ -2,7 +2,7 @@
 (() => {
   // تهيئة المتغيرات العامة
   const state = {
-    SHEETS_URL: "https://script.google.com/macros/s/AKfycbym9AdHT0ZRhuEXnrEfFIDCIYTKN9jshioRm8NhXcrh6Psqy--cD-ifoivmFAmUuxiEwQ/exec",
+    SHEETS_URL: "https://script.google.com/macros/s/AKfycbwYCb_Nbt-Dx1N3mPvMYIxhM9zvTOv7z3rleMo3NDbsssG5frkQXrIO3rG0Mt35o44-Mg/exec",
     lastSyncTime: 0,
     syncCheckInterval: null,
     lastRowCount: 0,
@@ -395,4 +395,5 @@
   }
 
   console.log('✅ تم تهيئة Google Sheets Sync مع المراقبة التلقائية');
+
 })();
