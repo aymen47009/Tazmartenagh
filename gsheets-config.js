0cf537854cf2fb9e241c6d901c4dd393ec1e3862
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2025-10-24 16:15:28 +0100

    Update gsheets-config.js

--- gsheets-config.js
+++ gsheets-config.js
@@ -6,8 +6,9 @@
 
 window.gsheetSyncConfig = {
   enabled: true,
-  endpointUrl: "https://script.google.com/macros/s/AKfycbw-FnluoGwlRVxyPaF7SWu2NytxdxKxMV62ZFr3iNCHaAR4l67nbXoLFueKUa7wf0J_mg/exec"
+  endpointUrl: "https://script.google.com/macros/s/AKfycbzcQYP3pzVywy1Ow2Jj4TQmtc01FerQRaBlsRkYiqJ7iYB7vfev7cgXsLCbUFP-b36MBg/exec"
 };
+
 
 
 
