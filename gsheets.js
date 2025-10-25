5b06c9733679ffd463d8d1878363327fde127471
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2025-10-25 08:23:50 +0100

    Update Google Sheets URL for synchronization

--- gsheets.js
+++ gsheets.js
@@ -2,7 +2,7 @@
 (() => {
   // تهيئة المتغيرات العامة
   const state = {
-    SHEETS_URL: "https://script.google.com/macros/s/AKfycbzmgeu1WRZc_ghMAd5ryMbpnaNOVcKSSRpdwi8Jjm5S62kRu-9LxwhgAyUCj-uOUZhoDg/exec",
+    SHEETS_URL: "https://script.google.com/macros/s/AKfycbxiva8TpiOvhsPLZYnIiYdvOAO-ik7R0YtSSjd80ZMdqosq4PF9QKeNTojExAreQE1N5Q/exec",
     lastSyncTime: 0,
     syncCheckInterval: null,
     lastRowCount: 0,
@@ -397,6 +397,7 @@
   console.log('✅ تم تهيئة Google Sheets Sync مع المراقبة التلقائية');
 
 })();
+
 
 
 
