77725b0108e9ec0762ad882a592ab4f15be203b0
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2025-10-25 09:30:45 +0100

    Update endpoint URL in gsheets-config.js

--- gsheets-config.js
+++ gsheets-config.js
@@ -6,8 +6,9 @@
 
 window.gsheetSyncConfig = {
   enabled: true,
-  endpointUrl: "https://script.google.com/macros/s/AKfycbxr47p6876QQqL2UggB1n-iDDJuLLOxyi3-8ufsMmABCMYkbir5E3whGe2XMV66FBpJ5w/exec"
+  endpointUrl: "https://script.google.com/macros/s/AKfycbxPrLEZIsREQAVZzOGFwdXjCgoCppWkSQTCLkyCl2csCoz3AoPPJ0Yb55eT1WmFXbIp8Q/exec"
 };
+
 
 
 
